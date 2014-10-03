4318ac877eca3c3683a92280f877e271daab7b0b
djlinkstrom <djlinkstrom@gmail.com>
2014-10-03 08:30:14 -0400

    redesign

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,6 @@ module Shoplandia
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = true
-    config.assets.precompile =true
-    config.serve_static_assets = true
+    
   end
 end
