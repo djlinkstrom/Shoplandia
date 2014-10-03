91e3fe7dcdab6696c69c3e1e37679476f1775da0
djlinkstrom <djlinkstrom@gmail.com>
2014-10-02 23:49:36 -0400

    redesign

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Shoplandia::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -20,16 +21,14 @@ Shoplandia::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-
   config.serve_static_assets = true
-  
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -80,5 +79,10 @@ Shoplandia::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #config.action_mailer.default_url_options = { :host => 'http://pinteresting-commits.herokuapp.com/' }
+
+  
+
   Gibbon::API.api_key = ENV['MAILCHIMP_API_KEY']
 end
+
